2c0784759cf20b3146e67e2ca20a437edc5d44aa
nrgbistro <nolangelinas@gmail.com>
2021-07-12 17:46:16 -0400

    Add doc directory for gh-pages integration

---
+++ webpack.prod-config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "production",
+	entry: {
+		index: "./src/scripts/index.js",
+	},
+	output: {
+		path: path.resolve(__dirname, "doc"),
+		filename: "[name].bundle.js",
+		library: "myLibrary",
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Pond5",
+			favicon: "./src/images/favicon.ico",
+			template: "./src/index.html",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+};
